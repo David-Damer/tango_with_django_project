cbccdf7ed3e95a8ce1d3ad481426dc1d51428b40
David Damer <2266980D@student.gla.ac.uk>
2018-01-26 13:04:11 +0000

    up to 8.3

--- rango/views.py
+++ rango/views.py
@@ -42,6 +42,7 @@ def add_category(request):
             return index(request)
         else:
             print(form.errors)
+
     return render(request, 'rango/add_category.html',{'form': form})
 
 def add_page(request, category_name_slug):
@@ -62,6 +63,7 @@ def add_page(request, category_name_slug):
                 return show_category(request, category_name_slug)
         else:
             print(form.errors)
+            
     context_dict = {'form':form,'category':category}
     return render(request, 'rango/add_page.html', context_dict)
 
